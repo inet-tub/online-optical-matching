4bb73edb5a8f6865a04c45a778a3a904ad308cb6
Vamsi Addanki <vamsiaddanki666@gmail.com>
2026-05-07 02:20:14 -0400

    update

--- plots.py
+++ plots.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
+import matplotlib
 import sys
 #%%
 plots_dir = "plots/"
@@ -27,16 +28,16 @@ minValue={}
 maxValue={}
 # traces = ["pFabric"]
 # algs = ["deterministic", "oblivious-1","oblivious-2","oblivious-4","oblivious-16","oblivious-64", "static" , "offline"]
-algs = ["deterministic", "oblivious-1", "static" , "offline"]
+algs = ["deterministic", "offline", "static", "oblivious-1"]
 labels = algs
 
 # algNames = ["Greedy", "OBL", "OBL-2", "OBL-4", "OBL-16", "OBL-64", "S-OFF", "OFF"]
 algNames = ["Greedy", "OBL", "S-OFF", "OFF"]
 algNames = {}
 algNames["deterministic"]="Greedy"
-algNames["oblivious-1"]="Oblivious"
-algNames["static"]="Static Offline"
-algNames["offline"]="Offline"
+algNames["oblivious-1"]="Oblivious (OBL)"
+algNames["static"]="Static Offline (S-OFF)"
+algNames["offline"]="Offline (OFF)"
 
 numNodes = 32
 # lowalpha = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
@@ -119,10 +120,15 @@ rcParams.update({'font.size': 40})
 for trace in traces:
     markerindex=0
     dft = df[(df['trace']==trace)&(df['alg']=="pred")&(df['alpha']<=1024)]
+    dfSoff = df[(df['trace']==trace)&(df['alg']!="pred")&(df['alg']!="oblivious-2")&(df['alg']!="oblivious-4")&(df['alg']!="oblivious-16")&(df['alg']!="oblivious-64")&(df["alpha"]!=0)&(df['alpha']<=1024)]
+    dprime = dfSoff[(dfSoff["alg"]=="static")]
+    soff = list(dprime["cost"])[0]/10**3
     fig, ax = plt.subplots(1,1, figsize=(10, 9))
+    ax.set_prop_cycle(color=plt.cm.Greys(np.linspace(0.4, 0.9, 10)))
     for alpha in alphas:
         d = dft[(dft["alpha"]==alpha)]
         d = d.sort_values(by="error", ascending=True)
+        print(d,alpha)
         ax.plot(2*d["error"], d['cost']/10**3,label=r'$\alpha =$'+str(alpha), lw = 4,marker=markers[markerindex],markersize=20)
         markerindex=markerindex+1
         # print(markerindex)
@@ -152,6 +158,8 @@ for trace in traces:
     ax.set_ylabel(r'Total cost')
     ax.set_xticks(2*d["error"])
     ax.set_xticklabels(2*d["error"],rotation=40)
+    
+    ax.axhline(soff,ls='--',c='r',label="Static Offline (S-OFF)")
     # ax.legend(framealpha=0.1)
     fig.tight_layout()
     
@@ -165,7 +173,7 @@ for trace in traces:
         ncol=len(labels),
         frameon=False,
         fontsize=18,
-        handlelength=2,
+        handlelength=1.2,
         markerscale=1
     )
     fig_legend.tight_layout()
